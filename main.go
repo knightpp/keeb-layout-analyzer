5de5de0dae2dcfa0840bd6fb7530796fe7b9cd27
Danylo Kondratiev <knightpp@proton.me>
2023-11-22 23:05:54 +0200

    Rewrite main.go

--- main.go
+++ main.go
@@ -2,24 +2,73 @@ package main
 
 import (
 	_ "embed"
+	"flag"
 	"fmt"
+	"log"
 
 	"github.com/knightpp/keeb-layout-analyzer/internal/layout"
 )
 
-//go:embed data/english_common_words.txt
-var engCommonWords string
+var (
+	//go:embed data/english_common_words.txt
+	engCommonWords string
+
+	//go:embed data/ukr.txt
+	ukrainian string
+)
+
+var (
+	fWords  string
+	fLayout string
+)
+
+func init() {
+	flag.StringVar(&fWords, "words", "ukr.txt", "specify path to a file or one of built-ins")
+	flag.StringVar(&fLayout, "layout", "ukr34", "layout to use")
+}
 
 func main() {
-	l := layout.New(layout.ColemakDH34Keys, &layout.State{
+	flag.Parse()
+
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	var keys []layout.Key
+	switch fLayout {
+	case "ukr34":
+		keys = layout.Ukrainian34
+	case "colemakdh":
+		keys = layout.ColemakDH34Keys
+	default:
+		return fmt.Errorf("unknown layout: %q", fLayout)
+	}
+
+	var words string
+	switch fWords {
+	case "ukr.txt":
+		words = ukrainian
+	case "english_common_words.txt":
+		words = engCommonWords
+	default:
+		return fmt.Errorf("unimplemented")
+	}
+
+	l := layout.New(keys, &layout.State{
 		Left:  &layout.Hand{},
 		Right: &layout.Hand{},
 	})
 
-	stats, err := l.Analyze(engCommonWords)
+	stats, err := l.Analyze(words)
 	if err != nil {
 		fmt.Println(err)
+		if errs, ok := err.(interface{ Unwrap() []error }); ok {
+			fmt.Println("total errors:", len(errs.Unwrap()))
+		}
 	}
 
 	fmt.Println(stats)
+	return nil
 }
